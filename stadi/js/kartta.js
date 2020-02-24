992650e7a200cfc140a605223b7666aa6c20f50d
Ilari Rauhala <ilari@Ilari-MacBook-Pro-2.local>
2020-02-24 13:20:58 +0200

    lisätty pelimuotoja, vähän nätimmäksi

--- stadi/js/kartta.js
+++ stadi/js/kartta.js
@@ -1,4 +1,7 @@
 var osat = [];
+var fresh = true;
+var osatt;
+var posti = false;
 
 function shuffle(array) {
   var m = array.length, t, i;
@@ -18,8 +21,63 @@ function shuffle(array) {
   return array;
 }
 
-
-for (osa of helosaalueet.features) {
-  osat.push({id:osa.properties.id, nimi_fi:osa.properties.nimi_fi, nimi_se:osa.properties.nimi_se});
+function nollaa() {
+  oikeat = 0;
+  vaarat = 0;
+  document.getElementById('voitit').innerHTML = '';
+  document.getElementById('oikeat').innerHTML = '';
+  document.getElementById('arvattava').innerHTML = '';
+  document.getElementById('vaarat').innerHTML = '';
+  if (! fresh){
+    hesa.removeLayer(osatt);
+  }
 }
-shuffle(osat);
+
+function alustaPeli(helosaalueet) {
+  nollaa();
+  fresh = false;
+  osat = [];
+  for (osa of helosaalueet.features) {
+    osat.push({id:osa.properties.id, nimi_fi:osa.properties.nimi_fi, nimi_se:osa.properties.nimi_se});
+  }
+  shuffle(osat);
+  osatt = L.geoJson(helosaalueet, {
+    style: neutraali
+  }).addTo(hesa);
+  osatt.eachLayer(function (layer) {
+            layer.on('click', function(ev) {
+              if (layer.feature.properties.id == osa.id) {
+                oikeat += 1;
+                document.getElementById('oikeat').innerHTML = 'Olet tiennyt ' + oikeat + ' kaupunginosaa!';
+                if (osa = osat.pop()){
+                  document.getElementById('arvattava').innerHTML = osa.nimi_fi + '<br>' + osa.nimi_se;
+                } else {
+                  document.getElementById('voitit').innerHTML = 'Voitit pelin!';
+                }
+                layer.bringToFront()
+                layer.setStyle(oikea);
+                osatt.eachLayer(function (ll) {
+                  if (vaarin.find(e => e === ll.feature.properties.id)){
+                    ll.setStyle(neutraali);
+                  }
+                })
+                vaarin = [];
+                this.redraw();
+              }
+              else if (osat.find(e => e.id === layer.feature.properties.id)) {
+                layer.setStyle(vaara);
+                layer.bringToFront()
+                vaarin.push(layer.feature.properties.id);
+                vaarat += 1;
+                document.getElementById('vaarat').innerHTML = 'Vääriä arvauksia: ' + vaarat;
+              }
+
+            ;});
+        });
+  var osa = osat.pop();
+//  if (posti) {
+//    document.getElementById('arvattava').innerHTML = osa.tunnus;
+//  } else {
+    document.getElementById('arvattava').innerHTML = osa.nimi_fi + '<br>' + osa.nimi_se;
+//  }
+}
