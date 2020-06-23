fabe7cb7b4164f09367d9f2d93e9894d3f4bf22c
Ilari Rauhala <ilari@Ilari-MacBook-Pro-2.local>
2020-06-23 20:30:39 +0300

    tarkempi gps

--- pikasuunnistus/suunnistus.js
+++ pikasuunnistus/suunnistus.js
@@ -8,6 +8,7 @@ var lcontrol;
 var virhe = 0;
 
 function dp(position) {
+    console.log(position);
     virhe = 0;
     for (l of rata.getLayers()) {
         kartta.removeLayer(l);
@@ -93,3 +94,52 @@ function lonToM(lon, lat) {
     let a = 111320 * Math.cos(lat/180*Math.PI);
     return lon * a;
 }
+
+// https://github.com/gregsramblings/getAccurateCurrentPosition
+navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess, geolocationError, geoprogress, options) {
+    var lastCheckedPosition,
+        locationEventCount = 0,
+        watchID,
+        timerID;
+
+    options = options || {};
+
+    var checkLocation = function (position) {
+        lastCheckedPosition = position;
+        locationEventCount = locationEventCount + 1;
+        // We ignore the first event unless it's the only one received because some devices seem to send a cached
+        // location even when maxaimumAge is set to zero
+        if ((position.coords.accuracy <= options.desiredAccuracy) && (locationEventCount > 1)) {
+            clearTimeout(timerID);
+            navigator.geolocation.clearWatch(watchID);
+            foundPosition(position);
+        } else {
+            geoprogress(position);
+        }
+    };
+
+    var stopTrying = function () {
+        navigator.geolocation.clearWatch(watchID);
+        foundPosition(lastCheckedPosition);
+    };
+
+    var onError = function (error) {
+        clearTimeout(timerID);
+        navigator.geolocation.clearWatch(watchID);
+        geolocationError(error);
+    };
+
+    var foundPosition = function (position) {
+        geolocationSuccess(position);
+    };
+
+    if (!options.maxWait)            options.maxWait = 10000; // Default 10 seconds
+    if (!options.desiredAccuracy)    options.desiredAccuracy = 20; // Default 20 meters
+    if (!options.timeout)            options.timeout = options.maxWait; // Default to maxWait
+
+    options.maximumAge = 0; // Force current locations only
+    options.enableHighAccuracy = true; // Force high accuracy (otherwise, why are you using this function?)
+
+    watchID = navigator.geolocation.watchPosition(checkLocation, onError, options);
+    timerID = setTimeout(stopTrying, options.maxWait); // Set a timeout that will abandon the location loop
+};
